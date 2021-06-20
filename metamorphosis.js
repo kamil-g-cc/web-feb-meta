399eb0b64c4ebfb579320b406ba46b069f772c30
Kamil Grondys <kgrondys@gmail.com>
2021-06-20 10:59:08 +0200

    task 1 completed

--- metamorphosis.js
+++ metamorphosis.js
@@ -3,7 +3,7 @@ initDragAndDrop();
 function initDragAndDrop() {
     shuffleCards();
     setDraggable(document.querySelectorAll('.card'));
-
+    setDragStartAndDragEnd(document.querySelectorAll('.card'));
     // Initialize drag & drop elements here
 
 }
@@ -22,4 +22,20 @@ function setDraggable(list){
     for(let i = 0; i < list.length; i++){
         list[i].setAttribute("draggable", true);
     }
+}
+function setDragStartAndDragEnd(list){
+    list.forEach(function(el){
+        el.addEventListener("dragstart", onDragStartHandler);
+        el.addEventListener("dragend", onDragEndHandler);
+    })
+}
+
+function onDragStartHandler(event){
+    //console.debug(event.currentTarget+" "+event.target);
+    event.target.classList.add('card-dragged');
+}
+
+function onDragEndHandler(event){
+    //console.debug(event.currentTarget+" "+event.target);
+    event.target.classList.remove('card-dragged');
 }
