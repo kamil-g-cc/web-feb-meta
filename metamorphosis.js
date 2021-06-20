88a74ea977c4c9ed3e5a30d90da2bb20b967bf6b
Kamil Grondys <kgrondys@gmail.com>
2021-06-20 11:32:33 +0200

    All .card-slot elements are active and visually highlighted when you pick up a card

--- metamorphosis.js
+++ metamorphosis.js
@@ -4,6 +4,7 @@ function initDragAndDrop() {
     shuffleCards();
     setDraggable(document.querySelectorAll('.card'));
     setDragStartAndDragEnd(document.querySelectorAll('.card'));
+
     // Initialize drag & drop elements here
 
 }
@@ -33,9 +34,44 @@ function setDragStartAndDragEnd(list){
 function onDragStartHandler(event){
     //console.debug(event.currentTarget+" "+event.target);
     event.target.classList.add('card-dragged');
+    togleActive();
 }
 
 function onDragEndHandler(event){
     //console.debug(event.currentTarget+" "+event.target);
     event.target.classList.remove('card-dragged');
+    togleActive(false);
 }
+
+function togleActive(switchOn=true){
+    //document.querySelectorAll('.metamorphosis-slots > div')
+    //document.querySelectorAll('.metamorphosis-slots > div.card-slot')
+    //document.querySelectorAll('div.metamorphosis-slots > div.card-slot')
+    //document.querySelectorAll('.card-slot')
+    let list = document.querySelectorAll('.card-slot');
+    if(switchOn){
+        list.forEach(function(el){
+            el.classList.add('card-slot-active');
+        })
+    } else {
+        list.forEach(function(el){
+            el.classList.remove('card-slot-active');
+        })
+    }
+}
+/*
+
+function setDragEnterAndLeave(list){
+    list.forEach(function(el){
+        el.addEventListener("dragenter", onDragEnterHandler);
+        el.addEventListener("dragleave", onDragLeaveHandler);
+    })
+}
+
+function onDragEnterHandler(event){
+
+}
+
+function onDragLeaveHandler(event){
+
+}*/
